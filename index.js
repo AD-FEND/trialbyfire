bc5f5bdd609c2ba1b9ac95d2b5c8eb5815a1e476
Amaad Daad <amaaddaad75@gmail.com>
2024-01-12 16:19:09 +0000

    Total sum

--- index.js
+++ index.js
@@ -15,6 +15,24 @@ function appendNumberToNumberList() {
     newListItem.textContent = `Random Number: ${randomNumber}`;
     const numberedList = document.getElementById("numbered-list");
     numberedList.appendChild(newListItem);
+
+    // Update the total.
+    let currentSum = 0
+    const listItems = document.querySelectorAll("#numbered-list li")
+    for (let i = 0; i < listItems.length; i++) {
+        const splitText = listItems[i].textContent.split(":")
+        const lastIndex = splitText.length-1;
+        const numberText = splitText[lastIndex].trim();
+        const numberValue = parseInt(numberText);
+        currentSum += numberValue;
+
+        console.log({currentSum,numberValue})
+    }
+
+
+    const elTotalAmountParagraph = document.querySelector("#total-amount");
+    elTotalAmountParagraph.textContent = `Total Amount: ${currentSum}`;
+
 }
 // Get the random number to come under the number list section.
 
